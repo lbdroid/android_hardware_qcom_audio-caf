409569baeef580a59676fea7a7ad0c0de0d2f6ec
lbdroid <lbdroid@users.noreply.github.com>
2014-11-24 15:23:24 -0500

    I don't think that that does what the author thinks it does...

--- legacy/alsa_sound/Android.mk
+++ legacy/alsa_sound/Android.mk
@@ -11,9 +11,6 @@ common_cflags := -D_POSIX_SOURCE
 ifneq ($(strip $(QCOM_ACDB_ENABLED)),false)
     common_cflags += -DQCOM_ACDB_ENABLED
 endif
-ifneq ($(strip $(QCOM_ANC_HEADSET_ENABLED)),false)
-    common_cflags += -DQCOM_ANC_HEADSET_ENABLED
-endif
 ifeq ($(strip $(QCOM_MULTI_VOICE_SESSION_ENABLED)),true)
     LOCAL_CFLAGS += -DQCOM_MULTI_VOICE_SESSION_ENABLED
 endif
